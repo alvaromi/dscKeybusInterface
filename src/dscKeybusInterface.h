a8b76fb9470f96ec679864aabd78e8c9691b4702
Alvaro Miranda <alvarom@outlook.com>
2020-10-25 13:47:12 -0700

    2 partitions and 8 zones

--- src/dscKeybusInterface.h
+++ src/dscKeybusInterface.h
@@ -26,9 +26,9 @@ const byte dscPartitions = 4;   // Maximum number of partitions - requires 19 by
 const byte dscZones = 4;        // Maximum number of zone groups, 8 zones per group - requires 6 bytes of memory per zone group
 const byte dscBufferSize = 10;  // Number of commands to buffer if the sketch is busy - requires dscReadSize + 2 bytes of memory per command
 #elif defined(ESP8266)
-const byte dscPartitions = 8;
-const byte dscZones = 8;
-const byte dscBufferSize = 50;
+const byte dscPartitions = 2;
+const byte dscZones = 1;
+const byte dscBufferSize = 100;
 #endif
 
 const byte dscReadSize = 16;   // Maximum size of a Keybus command
